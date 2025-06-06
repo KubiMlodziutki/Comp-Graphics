3e1f606bfd774003ab4fd117ca149c4248a25b5f
Jakub Gańko <128095396+KubiMlodziutki@users.noreply.github.com>
2025-06-06 06:42:07 +0200

    Update Vector3D.java

--- ball_renderer/src/Vector3D.java
+++ ball_renderer/src/Vector3D.java
@@ -1,13 +1,10 @@
 public class Vector3D {
-
     public final double x, y, z;
 
-    /* --- konstruktor --- */
     public Vector3D(double x, double y, double z) {
         this.x = x;  this.y = y;  this.z = z;
     }
 
-    /* --- operacje wektorowe --- */
     public Vector3D add(Vector3D v) {
         return new Vector3D(x + v.x, y + v.y, z + v.z);
     }
@@ -20,17 +17,14 @@ public class Vector3D {
         return new Vector3D(x * k, y * k, z * k);
     }
 
-    /** iloczyn skalarny */
     public double dot(Vector3D v) {
         return x * v.x + y * v.y + z * v.z;
     }
 
-    /** długość (norma Euklidesowa)  <<< NOWOŚĆ */
     public double length() {
         return Math.sqrt(this.dot(this));
     }
 
-    /** normalizacja do wektora jednostkowego */
     public Vector3D normalize() {
         double len = length();
         return (len == 0) ? this : scale(1.0 / len);
